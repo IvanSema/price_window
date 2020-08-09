7c13f4fc02294d9f0c7091c565705f2a56a6af0d
IvanSema <ivan_8d@ukr.net>
2020-08-09 22:12:27 +0300

    temp

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react'
+// @ts-ignore
+import ProductEquipment from '../src/components/product-equipment'
+
+const temp = [{'type':500, 'price': 500}, {'type':100, 'price': 150}, {'type': 50, 'price': 90}];
 
 const TestApp = () => (
-  <h1>Change me</h1>
+  <ProductEquipment array={temp} />
 )
 
 export default TestApp
